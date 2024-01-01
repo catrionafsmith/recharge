b84c2b3d764db6030fcd67c6c8622826c6b09424
Katy Smith <catriona.f.smith@gmail.com>
2024-01-01 20:23:42 +0000

    Build

--- recharge-app/src/components/TimedCarousel.tsx
+++ recharge-app/src/components/TimedCarousel.tsx
@@ -1,36 +1,40 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import Carousel from "react-bootstrap/Carousel";
 import Card from "react-bootstrap/Card";
 import exercises from "../exercises.json";
 import { useStopwatch } from "react-timer-hook";
-import useSound from 'use-sound';
-import whoosh from "/whoosh.mp3"
+// import useSound from 'use-sound';
+// import whoosh from "/whoosh.mp3"
 
 function TimedCarousel() {
   const { seconds, reset } = useStopwatch({ autoStart: true });
-  const [play, { stop }] = useSound(whoosh);
-  const [userHasInteracted, setUserHasInteracted] = useState(false);
-
-  useEffect(() => {
-    const unlockAudio = () => {
-      play();
-      stop();
-      setUserHasInteracted(true);
-      window.removeEventListener('click', unlockAudio);
-    };
-
-    window.addEventListener('click', unlockAudio);
-    return () => {
-      window.removeEventListener('click', unlockAudio);
-    };
-  }, [play, stop]);
-
-  const handleSlideChange = () => {
+  const handleSlideChange = (_eventKey: number, _direction: "end" | "start") => {
+    // You can use eventKey and direction if needed, or just ignore them
     reset();
-    if (userHasInteracted) {
-      play();
-    }
   };
+  // const [play, { stop }] = useSound(whoosh);
+  // const [userHasInteracted, setUserHasInteracted] = useState(false);
+
+  // useEffect(() => {
+  //   const unlockAudio = () => {
+  //     play();
+  //     stop();
+  //     setUserHasInteracted(true);
+  //     window.removeEventListener('click', unlockAudio);
+  //   };
+
+  //   window.addEventListener('click', unlockAudio);
+  //   return () => {
+  //     window.removeEventListener('click', unlockAudio);
+  //   };
+  // }, [play, stop]);
+
+  // const handleSlideChange = () => {
+  //   reset();
+  //   if (userHasInteracted) {
+  //     play();
+  //   }
+  // };
 
   return (
     <>
