85d93fa7d516253803455c33294229876ddb724e
Katy Smith <catriona.f.smith@gmail.com>
2025-03-17 21:50:02 +0000

    Add joke api call

--- recharge-app/src/components/ChatForm.tsx
+++ recharge-app/src/components/ChatForm.tsx
@@ -1,8 +1,13 @@
 import { useForm } from '@mantine/form';
-import { TextInput, Button } from '@mantine/core';
+import { Text, TextInput, Button, Stack, Group } from '@mantine/core';
+import axios from 'axios';
+import { useState } from 'react';
 
 
 export default function ChatForm() {
+
+  const [apiResponse, setApiResponse] = useState<string>('');
+  
   const form = useForm({
     mode: 'uncontrolled',
     initialValues: {
@@ -11,8 +16,25 @@ export default function ChatForm() {
     },
   });
 
+  console.log(form)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get('https://official-joke-api.appspot.com/random_joke', {});
+      setApiResponse(response.data.setup);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+      throw error;
+    }
+  };
+
+
   return (
-    <form onSubmit={form.onSubmit((values) => console.log(values))}>
+    <>
+
+    <Stack gap = {'md'}>
+    <form onSubmit={handleSubmit}>
         <TextInput
             label="Name"
             placeholder="Name"
@@ -20,12 +42,17 @@ export default function ChatForm() {
             {...form.getInputProps('name')}
             />
         <TextInput
-            label="Name"
+            label="Question"
             placeholder="Question"
             key={form.key('question')}
             {...form.getInputProps('question')}
             /> 
-        <Button type='submit'>Submit question</Button>
+            <Group justify={'flex-end'} mt='md'>
+              <Button type='submit'>Submit question</Button>
+            </Group>
     </form>
+    </Stack>
+    <Text mt={'md'}>{apiResponse}</Text>
+    </>
   );
 }
