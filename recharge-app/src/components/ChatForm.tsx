7357f3eb3b00b65421300aade189106a2d33e4f4
Katy Smith <113362369+catrionafsmith@users.noreply.github.com>
2025-03-17 11:38:18 +0000

    Create ChatForm.tsx

---
+++ recharge-app/src/components/ChatForm.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useForm } from '@mantine/form';
+import { TextInput, Button, Group } from '@mantine/core';
+
+
+export default function ChatForm() {
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      name: '',
+      question: '',
+    },
+  });
+
+  return (
+    <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <TextInput
+            label="Name"
+            placeholder="Name"
+            key={form.key('name')}
+            {...form.getInputProps('name')}
+            />
+        <TextInput
+            label="Name"
+            placeholder="Question"
+            key={form.key('question')}
+            {...form.getInputProps('question')}
+            /> 
+        <Button type='submit'>Submit question</Button>
+    </form>
+  );
+}
